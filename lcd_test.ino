aa40f831365dfcb8673a54f42ee8d58acf927d17
Krxwallo <ken.grill@gmx.de>
2022-06-04 14:27:49 +0200

    :boom: Migrate to I2C

--- lcd_test.ino
+++ lcd_test.ino
@@ -1,16 +1,13 @@
-#include <LiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  // Print a message to the LCD.
-  Serial.begin(9600);
-  Serial.println("START");
+  lcd.init();
+  lcd.backlight();
 }
 
 void loop() {
@@ -33,14 +30,14 @@ void loop() {
 }
 
 void printSlowely(String str, int delayMillis) {
-  for (int i = 0; i < str.length(); i++) {
+  for (unsigned short i = 0; i < str.length(); i++) {
     lcd.write(str[i]);
     delay(delayMillis);
   }
 }
 
 void printRandomized(String str, int minDelay, int maxDelay) {
-  for (int i = 0; i < str.length(); i++) {
+  for (unsigned short i = 0; i < str.length(); i++) {
     lcd.write(str[i]);
     int delayMillis = rand() % (maxDelay-minDelay+1) + minDelay;
     Serial.print("Delay: ");
